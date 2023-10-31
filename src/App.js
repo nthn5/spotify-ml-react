06ea872927c70180e0cf255cc1e79b10f762a2ab
nate <nathanbarton517@gmail.com>
2023-10-31 13:30:51 -0400

    creation of frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    authorize();
+  }, []);
+  
+  //Sign In Stuff
+  const signIn = async () => {
+    const client_id = localStorage.getItem('client_id');
+    // generates random string for auth flow
+    const generateRandomString = (length) => {
+      let text = '';
+      let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    
+      for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+    }
+
+    //hashes the code verifier
+    const generateCodeChallenge = async (codeVerifier) => {
+      function base64encode(string) {
+        return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+          .replace(/\+/g, '-')
+          .replace(/\//g, '_')
+          .replace(/=+$/, '');
+      }
+    
+      const encoder = new TextEncoder();
+      const data = encoder.encode(codeVerifier);
+      const digest = await window.crypto.subtle.digest('SHA-256', data);
+    
+      return [base64encode(digest), client_id];
+    }
+    
+    let codeVerifier = generateRandomString(128);
+
+    //redirects the user to the sign in screen and then back to application
+    generateCodeChallenge(codeVerifier).then(codeChallenge => {
+      let state = generateRandomString(16);
+      const scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-private';
+
+      localStorage.setItem('code_verifier', codeVerifier);
+
+      let args = new URLSearchParams({
+        response_type: 'code',
+        client_id: codeChallenge[1],
+        scope: scope,
+        redirect_uri: 'http://localhost:3000',
+        state: state,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge[0]
+      });
+
+      window.location = 'https://accounts.spotify.com/authorize?' + args;
+    });
+
+  }
+  const authorize = async () => {
+    const response = await fetch('http://localhost:5000/get_client_id');
+    const data = await response.json();
+    const client_id = data.client_id;
+    localStorage.setItem('client_id', client_id);
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+    let codeVerifier = localStorage.getItem('code_verifier');
+    
+    let body = new URLSearchParams({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: 'http://localhost:3000',
+      client_id: client_id,
+      code_verifier: codeVerifier
+    });
+
+    await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('HTTP status ' + response.status);
+      }
+      return response.json();
+    })
+    .then(async data => {
+      localStorage.setItem('access_token', data.access_token);
+      localStorage.setItem('refresh_token', data.refresh_token);
+      await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('access_token')
+        }
+      })
+      .then(res => res.json()
+      .then(data => localStorage.setItem('user_id', data.id)))
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
+  //Actual Functions
+  //-------------------------------------------------------------------------------------
+  const test = async () => {
+    await fetch('http://localhost:5000/test');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={signIn}>sign in</button>
+      <button onClick={test}>testing</button>
     </div>
   );
 }
